dce10b92bde88d661fdac2fb52c46636034a8ae1
Mukuakuiza <42307213+Mukuakuiza@users.noreply.github.com>
2024-04-29 02:54:32 +0200

    created a not found page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import {
 import Home from './pages/Home.jsx';
 import MainLayout from './layouts/MainLayout.jsx';
 import JobsPage from './pages/JobsPage.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout/>}>
     <Route index element={<Home/>}/>
     <Route path='/jobs' element={<JobsPage/>}/>
+    <Route path='*' element={<NotFound/>}/>
   </Route>
   
 )
