7178182f0ac6ce99b63523951427e08d78e0e9ad
Mukuakuiza <hinomukuakuiza12@gmail.com>
2024-05-27 01:17:21 +0200

    installed json-server, changed the json file,and jobs.json. installed spinner for loading

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,24 +1,45 @@
-import jobs from '../Jobs.json'
-import Job from './Job.jsx'
-const JobList = ({isHome = false}) => {
+import { useState, useEffect } from 'react';
+import Job from './Job.jsx';
+import Spinner from './Spinner.jsx';
+const JobList = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  //to show only 3 jobs
-    const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
-        <div className="container-xl lg:container m-auto">
-            <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                {isHome ? 'New Jobs': 'All Jobs'}
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {jobListings.map((job)=> (
-                    <Job key={job.id} job={job}/>
-                ))}   
-            </div>
-        </div>
-    </section>
-  )
-}
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? 'New Jobs' : 'All Jobs'}
+        </h2>
 
-export default JobList
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <Job key={job.id} job={job} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default JobList;
