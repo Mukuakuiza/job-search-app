fc6823d3ed5261e64456d46dec27ba21607fd00a
Mukuakuiza <hinomukuakuiza12@gmail.com>
2024-05-30 03:55:02 +0200

    created job single page, used useparams to get the job id, and get job with dataloader by the id

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import {
-  Route,createBrowserRouter,createRoutesFromElements,RouterProvider 
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
 } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import MainLayout from './layouts/MainLayout.jsx';
 import JobsPage from './pages/JobsPage.jsx';
 import NotFound from './pages/NotFound.jsx';
+import JobPage, { jobLoader } from './pages/JobPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-  <Route path='/' element={<MainLayout/>}>
-    <Route index element={<Home/>}/>
-    <Route path='/jobs' element={<JobsPage/>}/>
-    <Route path='*' element={<NotFound/>}/>
-  </Route>
-  
-)
-)
-
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return <RouterProvider router={router}/>
-}
+  return <RouterProvider router={router} />;
+};
 
-
-export default App
+export default App;
