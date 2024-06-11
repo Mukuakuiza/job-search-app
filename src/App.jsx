b6071e4cd25794d99c192d8e8ca7d7cfcdcea5fa
Mukuakuiza <hinomukuakuiza12@gmail.com>
2024-06-12 01:18:14 +0200

    add job to the existing list of jobs

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,33 @@ import MainLayout from './layouts/MainLayout.jsx';
 import JobsPage from './pages/JobsPage.jsx';
 import NotFound from './pages/NotFound.jsx';
 import JobPage, { jobLoader } from './pages/JobPage.jsx';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<Home />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
+import AddJob from './pages/AddJob.jsx';
 
 const App = () => {
+  //Add new job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJob addJobSubmitted={addJob} />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 };
 
